5d63d1235c4ebc4e8f2e5979f5b87d666d822227
romaxwel <rodrigo@mitis.com.br>
2013-03-26 16:04:58 -0300

    blz

--- HelloGIT2/HelloGIT2/Form1.vb
+++ HelloGIT2/HelloGIT2/Form1.vb
@@ -4,6 +4,8 @@
     '-0---tttt
     'ddd
     'sdfasdfa sf
+    '---blz...
+
     Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
 
     End Sub
